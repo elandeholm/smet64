8baa2b588ed3795c6eb1fa653cf2f4f34e37c763
elandeholm <emanuel.landeholm@gmail.com>
2012-08-06 17:44:59 +0200

    Factored out test code

--- smet64.c
+++ smet64.c
@@ -1,6 +1,5 @@
 #include <inttypes.h>
 #include <string.h>
-#include <stdio.h>
 
 #include "smet64.h"
 
@@ -158,63 +157,5 @@ void smet64_char(char block[16], struct smet64 *s64)
 			b64 >>= 8;
 		}
 	}	
-}
-
-void print_block(const char *pre, const char block[16])
-{
-	int i;
-	printf("%s", pre);
-	
-	for(i = 0; i < 16; ++i)
-	{
-		printf("%02x", (unsigned char)block[i]);
-	}
-	puts("");
-}
-
-void print_s64(const struct smet64 *s64)
-{
-	int i;
-	
-	puts("smet64 RK[], C[]");
-	for(i = 0; i < SMET64_NROUNDS+1; ++i)
-	{
-		printf("  %02d: %016"PRIx64" %016"PRIx64"\n", i, s64->RK[i], s64->C[i]);
-	}
-}
-
-int main(int c, char **v)
-{
-	char *p = "my secret passwd";
-	char block[16], facit[16];
-	struct smet64 s64;
-	
-	if(c > 0)
-	{
-		p = v[0];
-	}
-
-	memset(block, -1, sizeof(block));
-	memset(facit, -1, sizeof(facit));
-//	strcpy(block, "abcdefghijklmno");
-//	strcpy(facit, block);
-	print_block("plain: ", block);	
-//	print_s64(&s64);
-	smet64_decode_init(p, &s64);
-	smet64_char(block, &s64);
-	print_block("  dec: ", block);
-	smet64_char(block, &s64);
-	memset(block, -1, sizeof(block));
-	smet64_encode_init(p, &s64);
-	smet64_char(block, &s64);
-	print_block("  enc: ", block);
-//	print_s64(&s64);
-	smet64_decode_init(p, &s64);
-	smet64_char(block, &s64);
-	print_block("  dec: ", block);
-	printf("encryption/decryption verification: %s\n",
-		memcmp(facit, block, sizeof(block)) ? "FAIL!" : "ok");
-
-	return 0;
 }
 
