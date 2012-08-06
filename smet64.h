4cae49020d9bb5ec51a76044951796391720947d
elandeholm <emanuel.landeholm@gmail.com>
2012-08-06 16:18:32 +0100

    smet64 api def, init commit

---
+++ smet64.h
@@ -0,0 +1,13 @@
+#define NROUNDS 20
+
+struct smet64
+{
+	uint64_t RK[NROUNDS+1];
+	uint64_t C[NROUNDS+1];
+};
+
+void smet64_encode_init(const char *p, struct smet64 *s64);
+void smet64_decode_init(const char *p, struct smet64 *s64);
+void smet64_block64(uint64_t block64[2], struct smet64 *s64);
+void smet64_char(char block[16], struct smet64 *s64);
+
